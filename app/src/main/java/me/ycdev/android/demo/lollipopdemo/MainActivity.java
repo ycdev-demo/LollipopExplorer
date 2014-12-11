fb6457458c29711b776aa89930db09472595e38e
Yongce Tu <yongce.tu@gmail.com>
2014-12-11 16:21:41 +0800

    change: Add new notification feature and material design style (colors)

--- app/src/main/java/me/ycdev/android/demo/lollipopdemo/MainActivity.java
+++ app/src/main/java/me/ycdev/android/demo/lollipopdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package me.ycdev.android.demo.lollipopdemo;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +14,7 @@ import android.widget.Button;
 
 import me.ycdev.android.demo.lollipopdemo.usage.apps.AppsUsageViewerActivity;
 import me.ycdev.android.demo.lollipopdemo.usage.events.UsageEventsViewerActivity;
+import me.ycdev.android.demo.lollipopdemo.utils.Constants;
 
 
 public class MainActivity extends Activity {
@@ -61,8 +66,42 @@ public class MainActivity extends Activity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_notify) {
+            testNotification();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Notification getSimpleNotification() {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("simple title");
+        builder.setContentText("simple text");
+        return builder.build();
+    }
+
+    private void testNotification() {
+        Bitmap appIcon = ((BitmapDrawable) getDrawable(R.drawable.ic_launcher)).getBitmap();
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("test title");
+        builder.setContentText("test text");
+        builder.setSubText("Set the third line of text in the platform notification template.");
+        builder.setStyle(new Notification.BigTextStyle(builder).bigText("Helper class for generating" +
+                " large-format notifications that include a lot of text." +
+                " Here's how you'd set the BigTextStyle on a notification."));
+        builder.setSmallIcon(android.R.drawable.stat_notify_chat);
+        builder.addAction(android.R.drawable.ic_menu_send, "send", null);
+        builder.setLargeIcon(appIcon);
+        builder.setNumber(5);
+        // the following two lines to make the notification to be a "hands-up"
+        builder.setPriority(Notification.PRIORITY_MAX);
+        builder.setVibrate(new long[] {1, 2, 3, 4});
+        builder.setColor(0xffaa0000); // new API
+        builder.setPublicVersion(getSimpleNotification()); // new API
+        builder.setVisibility(Notification.VISIBILITY_SECRET); // new API
+        Notification notification = builder.build();
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(Constants.NOTIFICATION_TAG_APP, Constants.NOTIFICATION_ID_MAIN_TEST, notification);
+
     }
 }
---
+++ app/src/main/java/me/ycdev/android/demo/lollipopdemo/utils/Constants.java
@@ -0,0 +1,6 @@
+package me.ycdev.android.demo.lollipopdemo.utils;
+
+public class Constants {
+    public static final String NOTIFICATION_TAG_APP = "app";
+    public static final int NOTIFICATION_ID_MAIN_TEST = 1000;
+}
